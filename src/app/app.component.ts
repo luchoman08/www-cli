c4c37a80128a513d07a3126ef52f92d689e8a214
invitado <invitado@s4pc22.eisc.univalle.edu.co>
2017-09-12 15:49:47 -0500

    Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [HeroService],
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  heroes: Hero[];
+  title = 'My First Angular App';
+  selectedHero: Hero
+
+  constructor(private heroService: HeroService) { 
+    
+  }
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
+ 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+ 
+  onSelect(hero){
+    this.selectedHero = hero;
+  }
 }
+
+

6d843ca95a6bc72628d0091a2131220edadfbf3b
Lgmc <luis.manrique@correounivalle.edu.co>
2017-09-15 05:07:53 +0000

    angular historias y cordova intento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroDetailComponent }  from './hero-detail.component';
 import { HistoriasComponent } from './historias/historias.component';
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/historias', pathMatch: 'full' },
   { path: 'dashboard',  component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes',     component: HeroesComponent },
